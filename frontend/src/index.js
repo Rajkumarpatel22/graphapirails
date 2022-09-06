14274c6a83e6205f895d587b7a7f3e92e69f40b8
Rajkumar patel <deepti.sharma@ongraph.com>
2022-09-06 16:19:05 +0530

    update name

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,7 +7,7 @@ import { ApolloClient } from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
-const link = createHttpLink({ uri: 'http://localhost:3000' });
+const link = createHttpLink({ uri: 'http://localhost:3000/graphql' });
 const client = new ApolloClient({ link: link, cache: new InMemoryCache() });
 
 ReactDOM.render(
